c9210a0b42ebcd33022ce7604af9618309a9f209
DESKTOP-KTM0OPN\user <qkfdjq451@naver.com>
2019-05-08 00:50:37 +0900

    초기 커밋.

---
+++ LockFreeQueue/LockFreeQueue.h
@@ -0,0 +1,83 @@
+#pragma once
+#include <atomic>
+#include <array>
+#include <mutex>
+
+template<typename T, size_t size>
+class LockFreeQueue
+{
+public :
+	bool push(const T& newElement);
+	bool pop(T& returnedElement);
+private:
+	static constexpr size_t getPositionAftter(size_t pos) noexcept
+	{
+		return ++pos == ringBufferSize ? 0 : pos;
+	}
+	static constexpr size_t getPosition(size_t pos) noexcept
+	{
+		return pos >= ringBufferSize ? pos - ringBufferSize : pos;
+	}
+
+	static constexpr size_t ringBufferSize = size + 1;
+	std::array<std::atomic<T>, ringBufferSize> ringBuffer;
+	std::atomic<size_t> readPos = { 0 }, writePos = { 0 };
+	std::atomic<size_t> size = 0;
+	std::recursive_mutex readMutex;
+	std::recursive_mutex writeMutex;
+};
+
+template<typename T, size_t size>
+inline bool LockFreeQueue<T, size>::push(const T& newElement)
+{	
+	auto beforeSize = size.fetch_add(1);
+	if (beforeSize >= ringBufferSize)
+	{
+		size.fetch_sub(1);
+		return false;
+	}
+	auto beforePos = writePos.fetch_add(1);
+	if (beforePos >= ringBufferSize)
+	{
+		{
+			//buffer 오버플로가 나면 잠깐 락걸고 buffer size만큼 뺀다.
+			std::lock_guard<std::recursive_mutex> m(writeMutex);
+			auto current = writePos.load();
+			if (current >= ringBufferSize)
+			{
+				writePos.fetch_sub(ringBufferSize);
+			}
+		}
+		beforePos = getPosition(beforePos);
+	}
+	ringBuffer[beforePos].store(newElement);	
+	return true;
+}
+
+template<typename T, size_t size>
+inline bool LockFreeQueue<T, size>::pop(T& returnedElement)
+{
+	auto beforeSize = size.fetch_sub(1);
+	if (beforeSize <= 0)
+	{
+		size.fetch_add(1);
+		return false;
+	}
+
+	auto beforePos = readPos.fetch_add(1);
+	if (beforePos >= ringBufferSize)
+	{
+		{
+			//buffer 오버플로가 나면 잠깐 락걸고 buffer size만큼 뺀다.
+			std::lock_guard<std::recursive_mutex> m(readMutex);
+			auto current = readPos.load();
+			if (current >= ringBufferSize)
+			{
+				readPos.fetch_sub(ringBufferSize);
+			}
+		}
+		beforePos = getPosition(beforePos);
+	}
+	returnedElement = ringBuffer[beforePos].load();
+	return true;
+}
---
+++ LockFreeQueue/test.cpp
@@ -0,0 +1,172 @@
+#include "LockFreeQueue.h"
+#include <thread>
+#include <vector>
+#include <Windows.h>
+#include <chrono>
+#include <iostream>
+#include <concurrent_queue.h>
+#include <queue>
+#include <mutex>
+
+int main()
+{
+	constexpr size_t bufferSize = 100000;
+	constexpr size_t threadSize = 8;
+	LockFreeQueue<int, bufferSize> lockFreeQueueTest;
+	HANDLE hEvent;
+	hEvent = CreateEvent(nullptr, TRUE, TRUE, nullptr);
+	std::chrono::time_point<std::chrono::system_clock> now;
+	
+	//푸쉬 테스트
+	std::cout << "Push 속도 테스트 " << std::endl;
+	std::cout << "버퍼 사이즈(수행 수) : " << bufferSize << std::endl;
+	std::cout << "쓰레드 수 : " << threadSize << std::endl << std::endl;
+
+	for (int i = 0; i < threadSize; ++i)
+	{
+		std::vector<std::thread> threads;
+		for (int i = 0; i < threadSize; ++i)
+		{
+			threads.emplace_back(
+				[&] {
+					WaitForSingleObject(hEvent, INFINITE);
+					for (int i = 0; i < bufferSize / threadSize; ++i)
+					{
+						lockFreeQueueTest.push(i);
+					}
+				});
+		}
+		now = std::chrono::system_clock::now();
+		SetEvent(hEvent);
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - now);
+		std::cout << "내가 만든 락프리 푸시 소모시간 : " << duration.count() << std::endl;
+		
+		ResetEvent(hEvent);
+		threads.clear();
+		for (int i = 0; i < threadSize; ++i)
+		{
+			threads.emplace_back(
+				[&] {
+					WaitForSingleObject(hEvent, INFINITE);
+					int temp;
+					for (int i = 0; i < bufferSize / threadSize; ++i)
+					{
+						while(lockFreeQueueTest.pop(temp)==false);
+					}
+				});
+		}
+		now = std::chrono::system_clock::now();
+		SetEvent(hEvent);
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+		duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - now);
+		std::cout << "내가 만든 락프리 팝 소모시간 : " << duration.count() << std::endl << std::endl;
+	}
+
+	for (int i = 0; i < threadSize; ++i)
+	{
+		concurrency::concurrent_queue<int> t;
+		std::vector<std::thread> threads;
+		for (int i = 0; i < threadSize; ++i)
+		{
+			threads.emplace_back(
+				[&] {
+					WaitForSingleObject(hEvent, INFINITE);
+					for (int i = 0; i < bufferSize / threadSize; ++i)
+					{
+						t.push(i);
+					}
+				});
+		}
+		now = std::chrono::system_clock::now();
+		SetEvent(hEvent);
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - now);
+		std::cout <<"컨커런시 푸시 소모시간: "<< duration.count() << std::endl;
+
+		ResetEvent(hEvent);
+		threads.clear();
+		for (int i = 0; i < threadSize; ++i)
+		{
+			threads.emplace_back(
+				[&] {
+					WaitForSingleObject(hEvent, INFINITE);
+					int temp;
+					for (int i = 0; i < bufferSize / threadSize; ++i)
+					{
+						while (t.try_pop(temp) == false);
+					}
+				});
+		}
+		now = std::chrono::system_clock::now();
+		SetEvent(hEvent);
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+		duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - now);
+		std::cout << "컨커런시 팝 소모시간 : " << duration.count() << std::endl << std::endl;
+	}
+
+	{
+		std::queue<int> t;
+		std::recursive_mutex lock;
+		std::vector<std::thread> threads;
+		for (int i = 0; i < threadSize; ++i)
+		{
+			threads.emplace_back(
+				[&] {
+					WaitForSingleObject(hEvent, INFINITE);
+					for (int i = 0; i < bufferSize / threadSize; ++i)
+					{
+						std::lock_guard<std::recursive_mutex> m(lock);
+						t.push(i);
+					}
+				});
+		}
+		now = std::chrono::system_clock::now();
+		SetEvent(hEvent);
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - now);
+		std::cout << "락 큐 푸쉬 소모시간: " << duration.count() << std::endl;
+
+		ResetEvent(hEvent);
+		threads.clear();
+		for (int i = 0; i < threadSize; ++i)
+		{
+			threads.emplace_back(
+				[&] {
+					WaitForSingleObject(hEvent, INFINITE);
+					int temp;
+					for (int i = 0; i < bufferSize / threadSize; ++i)
+					{
+						std::lock_guard<std::recursive_mutex> m(lock);
+						temp = t.front();
+						t.pop();
+					}
+				});
+		}
+		now = std::chrono::system_clock::now();
+		SetEvent(hEvent);
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+		duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - now);
+		std::cout << "락 큐 팝 소모시간 : " << duration.count() << std::endl << std::endl;
+
+		system("pause");
+	}
+}
